48098a7d6b46ceb5a954aa6aaf194b2e58e91452
BegulaTheMoai <alexismod78520@gmail.com>
2026-08-19 02:52:24 +0200

    feat: connection (e2ee/dave impl is required for stable connection </3)

--- voice/voice.go
+++ voice/voice.go
@@ -1,6 +1,7 @@
 package voice
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/begulathemoai/metroballs/config"
@@ -21,77 +22,44 @@ func New(sesh *discordgo.Session, conf *config.Config) Voice {
 	}
 }
 
+// there must be a function somewhere in discordgo that does this but i haven't found it
+func getVoiceState(s *discordgo.Session, gID string, uID string) (j map[string](any), err error) {
+	out, err := s.Request("GET", "https://discord.com/api/v10/guilds/"+gID+"/voice-states/"+uID, nil)
+	if err != nil {
+		return nil, fmt.Errorf("while requesting voice state: %w", err)
+	}
+	var how_to_sleep map[string](any) // https://youtu.be/f5wrW7gv7b8
+	err = json.Unmarshal(out, &how_to_sleep)
+	if err != nil {
+		return nil, fmt.Errorf("while unmarshaling received voice state: %w", err)
+	}
+	return how_to_sleep, nil
+
+}
+
 // stupid name
 // i keep getting 401's :sob:
 func (v *Voice) DoConnect(s *discordgo.Session, i *discordgo.InteractionCreate, callerID string) (err error) {
-	out, _ := s.Client.Get("https://discord.com/api/v9/guilds/" + i.GuildID + "/voice-states/" + i.Member.User.ID)
-	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-		Type: discordgo.InteractionResponseChannelMessageWithSource,
-		Data: &discordgo.InteractionResponseData{
-			Flags:   discordgo.MessageFlagsEphemeral,
-			Content: fmt.Sprintf("getting channel `%v` in guild `%v` : `%v`", i.GuildID, i.Member.User.ID, out),
-		}})
-	/*chs, err := s.GuildChannels(i.GuildID)
-
-	for _, ch := range chs {
-		ch.Members
-	}*/
-	return nil
-
-	/*fmt.Printf("starting check")
-	if s.State == nil {
-		fmt.Printf("What The Fuck")
-		return nil
-	}
-	vc, err := s.State.VoiceState(i.GuildID, i.Member.User.ID)
-	if err != nil || vc == nil {
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Flags:   discordgo.MessageFlagsEphemeral,
-				Content: "no",
-			}})
-		return err
-	}
-
-	vc_id := vc.ChannelID
-
-	if vc_id == "" {
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Flags:   discordgo.MessageFlagsEphemeral,
-				Content: "you're not in a voice channel dumbass",
-			}})
-	} else {
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Flags:   discordgo.MessageFlagsEphemeral,
-				Content: fmt.Sprintf("found you in <#%v>", vc_id),
-			}})
-	}
-	return nil*/
-	/*chs, err := s.GuildChannels(v.Config.DiscordToken)
+	user_voice_state, err := getVoiceState(s, i.GuildID, i.Member.User.ID)
 	if err != nil {
-		return fmt.Errorf("getting all channels: %w", err)
-	}
 
-	// yeah this is stupid but counterpoint i don't know go
-	filtered_chs := make([]*discordgo.Channel,0,len(chs))
-	for _, ch := range chs {
-		if ch.Type == discordgo.ChannelTypeGuildVoice {
-			filtered_chs = append(filtered_chs, ch)
+		return fmt.Errorf("while getting voice state: %w", err)
+	}
+	voice_channel_id, ok := user_voice_state["channel_id"].(string)
+	if ok {
+		err := v.Connect(voice_channel_id, false, true)
+		if err != nil {
+			return fmt.Errorf("while connecting to voice: %w", err)
 		}
+	} else {
+		return fmt.Errorf("returned \"channel_id\" is not a string")
 	}
 
-	for _, ch := range filtered_chs {
-		if ch.
-	}*/
+	return nil
 }
 
-func (v *Voice) Connect(id int, muted bool, deafened bool) (err error) {
-	conn, err := v.Session.ChannelVoiceJoin(v.Config.DiscordGuildID, fmt.Sprint(id), muted, deafened)
+func (v *Voice) Connect(id string, muted bool, deafened bool) (err error) {
+	conn, err := v.Session.ChannelVoiceJoin(v.Config.DiscordGuildID, id, muted, deafened)
 	if err != nil {
 		v.Connection = conn
 		return nil
